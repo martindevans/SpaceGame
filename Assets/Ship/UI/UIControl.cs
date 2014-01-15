c82e77b367af4d37ca82adac7947a26dddd101fa
Chronojam <calumgardner23@gmail.com>
2014-01-15 14:24:38 +0000

    Added some basic UI scripts to faclitate increasing and decreasing engine power

---
+++ Assets/Ship/UI/UIControl.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestUI : MonoBehaviour {
+	public ShipMovement ShipMovementScript;
+	public ShipStation Parent;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	void OnMouseOver()
+	{
+
+	}
+	void OnMouseDown ()
+	{
+		if (!Parent.Active) {
+			Debug.Log ("Clicked!");
+			//ShipMovementScript.Thrust += 1000f;
+		}
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
